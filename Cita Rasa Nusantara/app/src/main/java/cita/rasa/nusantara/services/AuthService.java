2fe9b4ca449f7d1e05eb89b8d8b5d85857696dd5
mohfajar <mohfajarfajar48@gmail.com>
2026-05-30 06:02:20 -0700

    feat: tambah pengontrol verifikasi login

--- Cita Rasa Nusantara/app/src/main/java/cita/rasa/nusantara/services/AuthService.java
+++ Cita Rasa Nusantara/app/src/main/java/cita/rasa/nusantara/services/AuthService.java
@@ -1,5 +1,27 @@
-package cita.rasa.nusantara.services;
+package citarasa.services;
+
+import cita.rasa.nusantara.utils.DatabaseConfig;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AuthService {
     
+    public boolean login(String username, String password) {
+        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return rs.next();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
