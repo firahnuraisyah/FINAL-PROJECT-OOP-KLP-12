344f429a3c7c411d590954495dd73028c4d2a2f3
mohfajar <mohfajarfajar48@gmail.com>
2026-05-30 05:51:54 -0700

    feat: tambah fitur login

--- Cita Rasa Nusantara/app/src/main/java/cita/rasa/nusantara/controllers/LoginController.java
+++ Cita Rasa Nusantara/app/src/main/java/cita/rasa/nusantara/controllers/LoginController.java
@@ -1,5 +1,25 @@
 package cita.rasa.nusantara.controllers;
 
+import cita.rasa.nusantara.services.AuthService;
+import cita.rasa.nusantara.scene.ShowScene;
+import cita.rasa.nusantara.utils.AlertUtil;
+
 public class LoginController {
-    
+    private AuthService authService = new AuthService();
+
+    public void handleLogin(String username, String password) {
+        if (username.trim().isEmpty() || password.trim().isEmpty()) {
+            AlertUtil.showError("Login Gagal", "Username dan Password tidak boleh kosong!");
+            return;
+        }
+
+        boolean success = authService.login(username, password);
+
+        if (success) {
+            AlertUtil.showInfo("Login Berhasil", "Selamat Datang di Cita Rasa Nusantara, " + username + "!");
+            ShowScene.toHome();
+        } else {
+            AlertUtil.showError("Login Gagal", "Username atau Password salah.");
+        }
+    }
 }
--- Cita Rasa Nusantara/app/src/main/java/cita/rasa/nusantara/services/TableService.java
+++ Cita Rasa Nusantara/app/src/main/java/cita/rasa/nusantara/services/TableService.java
@@ -1,5 +1,43 @@
-package cita.rasa.nusantara.services;
+package citarasa.services;
+
+import citarasa.utils.DatabaseConfig;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TableService {
+
+  
+    public List<String> getAvailableTables() {
+        List<String> tables = new ArrayList<>();
+        String query = "SELECT table_number FROM restaurant_tables WHERE status = 'TERSEDIA'";
+        
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query);
+             ResultSet rs = pstmt.executeQuery()) {
+            
+            while (rs.next()) {
+                tables.add(rs.getString("table_number"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tables;
+    }
+
     
+    public void occupyTable(String tableNumber) {
+        String query = "UPDATE restaurant_tables SET status = 'DIGUNAKAN' WHERE table_number = ?";
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            
+            pstmt.setString(1, tableNumber);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
